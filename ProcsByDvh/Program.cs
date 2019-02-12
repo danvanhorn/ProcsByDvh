f25e052bb9c899f0c31325e0752797b26e187898
Dan Van Horn <dan.vanhorn17@gmail.com>
2019-02-12 07:31:53 -0800

    initial commit

--- ProcsByDvh/Program.cs
+++ ProcsByDvh/Program.cs
@@ -1,12 +1,50 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 
 namespace ProcsByDvh
 {
     class Program
     {
+        static void ListThreadsInProcess(string pid) {
+            List<Process> procs = new List<Process>(Process.GetProcesses(Environment.MachineName));
+            Process proc = procs.Find(p => String.Equals(p.Id.ToString(),pid));
+            foreach (ProcessThread thread in proc.Threads) {
+                Console.WriteLine(thread);
+            }
+        }
+
+        static void ListProcesses() {
+            Process[] procs = Process.GetProcesses(Environment.MachineName);
+            foreach (Process proc in procs) {
+                Console.WriteLine("pid: {0} name: {1}", proc.Id, proc.ProcessName);
+            }
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string line = null;
+            Console.Write("ProcsByDvh\n");
+            Console.Write("Press (1) to see a list of running processses\n");
+            Console.Write("Enter a PID and press enter\n");
+            Console.Write("Press (2) to list on running threads within the process boundary\n");
+            Console.Write("Press (3) to enumerate all loaded modules within a process\n");
+            Console.Write("Press (4) to show all executable pages within the process\n");
+            Console.Write("Press (5) to read process memory\n");
+            Console.Write("Type 'exit' to exit\n");
+            while (true) {
+                line = Console.ReadLine();
+                switch(line) {
+                    case "1":
+                        ListProcesses();
+                        continue;
+                    default:
+                        break;
+                } 
+                if (String.Equals(line, "exit")) {
+                    break;
+                }
+            }
         }
     }
 }
